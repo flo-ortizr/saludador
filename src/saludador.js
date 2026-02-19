44641889ad22661c93942230abdeb94e2c64593e
Flora <flora.ortiz@ucb.edu.bo>
2026-02-19 00:02:48 -0400

    feat: saludar de acuerdo a la hora

--- src/saludador.js
+++ src/saludador.js
@@ -1,5 +1,17 @@
 function saludar(nombre) {
-  return "Hola " + nombre;
+  let fechaActual = new Date();
+  let horaActual = fechaActual.getHours();
+  let saludo = "";
+
+  if (horaActual < 12) {
+    saludo = "Buenos días";
+  } else if (horaActual < 19) {
+    saludo = "Buenas tardes";
+  } else {
+    saludo = "Buenas noches";
+  }
+
+  return saludo + " " + nombre;
 }
 
 export default saludar;
